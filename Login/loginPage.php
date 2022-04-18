1fccb8784f015fc41907491fc023ee320faf4e06
Bushra Al-Hetelah <51024435+BushraH2000@users.noreply.github.com>
2022-04-18 23:11:41 +0300

    add action

--- Login/loginPage.php
+++ Login/loginPage.php
@@ -80,7 +80,7 @@
                     <p class="text-center small">Enter your username & password to login</p>
                   </div>
 
-                  <form class="row g-3 needs-validation" novalidate>
+                  <form action='' class="row g-3 needs-validation" novalidate>
 
                     <div class="col-12">
                       <label for="yourUsername" class="form-label">Username</label>
