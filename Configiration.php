6425cfe8cc4e320211fc2990ca1bf629aa2d0bc0
Bushra Al-Hetelah <51024435+BushraH2000@users.noreply.github.com>
2022-04-18 22:17:34 +0300

    Delete DB

--- Configiration.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define('ROOT_URL', 'http://localhost:6060/webSecurity-Project/MyResume/');
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'miners');
-	define('DB_PASS', 'Backhome@2');
-	define('DB_NAME', 'webdb');
-    
-?>
