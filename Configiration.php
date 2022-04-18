b9ba9a10c16152131604263ead51d4d5852dab78
Bushra Al-Hetelah <51024435+BushraH2000@users.noreply.github.com>
2022-04-18 20:30:58 +0300

    Configiration.php add

---
+++ Configiration.php
@@ -0,0 +1,8 @@
+<?php
+	define('ROOT_URL', 'http://localhost:6060/webSecurity-Project/MyResume/');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'miners');
+	define('DB_PASS', 'Backhome@2');
+	define('DB_NAME', 'crypto miners users');
+    
+?>
