5cf67e051636036cac658579c86d3297cacadea8
Bushra Al-Hetelah <51024435+BushraH2000@users.noreply.github.com>
2022-04-22 16:14:39 +0300

    add name into registration

--- Login/addUser.php
+++ Login/addUser.php
@@ -5,11 +5,12 @@
     if(isset($_POST['submit'])){
         // Get form data
         $ID = uniqid();
+        $name = mysqli_real_escape_string($conn, $_POST['name']);
         $username = mysqli_real_escape_string($conn, $_POST['username']);
         $password = mysqli_real_escape_string($conn, $_POST['password']);
         $email = mysqli_real_escape_string($conn, $_POST['email']);
      
-        $query = "INSERT users SET ID='$ID', username='$username', password='$password' , email='$email' , Pr='u' ";
+        $query = "INSERT users SET ID='$ID', name='$name' , username='$username', password='$password' , email='$email' , Pr='u' ";
         define('ROOT_URL', '../MyResume/home.php');
         if(mysqli_query($conn, $query)){
             header('Location: '.ROOT_URL.'');
