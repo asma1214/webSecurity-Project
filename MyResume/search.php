9c44a1be228e560f0638959c88738c951b657e0e
LayanAM <layan1117@gmail.com>
2022-04-22 22:20:37 +0300

    search

--- MyResume/search.php
+++ MyResume/search.php
@@ -61,17 +61,12 @@
     <!-- ======= Miners Section ======= -->
 
 
+
+
 <?php
 
 include('../connectDB.php');
 
-
- $query = "SELECT * FROM tools ORDER BY id";
- $result = mysqli_query($conn, $query);
- $tools = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
- mysqli_free_result($result);
- mysqli_close($conn);
 ?>
 
     <section id="miners" class="portfolio section-bg">
@@ -81,57 +76,68 @@ include('../connectDB.php');
           <h2>Miners</h2>
           <p>Want to start Mining in Saudi Arabia, Use our world class mining facility.</p>
         </div>
-
-        <div class="search-bar">
-      <form class="search-form d-flex align-items-center searchBar" action="search.php" method="POST" >
-        <!-- <input type="text" name="query" placeholder="Search" title="Enter search keyword"> -->
-        <!-- <button type="submit" title="Search"><i class="bi bi-search"></i></button> -->
-        <input type="search" name="search" required>
-        <i class="fa fa-search"></i>
-     </form>
-    </div>
-
-      <div class="row">
-          <div class="col-lg-12 d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
-            <ul id="portfolio-flters">
-              <li data-filter="*" class="filter-active">All</li>
-              <li data-filter=".filter-Antminer">Antminer</li>
-              <li data-filter=".filter-Avalon">Avalon</li>
-              <li data-filter=".filter-Bitmain">Bitmain</li>
-            </ul>
-          </div>
-        </div>
-
-
-
-
-
-<?php foreach($tools as $tool) : ?>
-        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
-          <div class="col-lg-4 col-md-6 portfolio-item ">
-            <div class="portfolio-wrap">
-              <img src="assets/img/miners/<?php echo $tool['img']; ?>" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4><?php echo $tool['itemName']; ?></h4>
-                <div class="portfolio-links">
-		
-
-                  <a href="assets/img/miners/<?php echo $tool['img']; ?>" data-gallery="portfolioGallery" class="portfolio-lightbox" title="<?php echo $tool['itemName']; ?>"><i class="bx bx-plus"></i></a>
-		    <a class="portfolio-details-lightbox" href="portfolio-details.php?id=<?php echo $tool['id']; ?>"class="portfolio-details-lightbox" data-glightbox="type: external" title="Miner Details"><i class="bx bx-link"></i></a>
-
-
-                </div>
-              </div>
-            </div>
-          </div>
-<?php endforeach; ?>	
-
-
-
          
         </div>
+        <div class="search-bar">
+          <form class="search-form d-flex align-items-center searchBar" action="search.php" method="POST" >
+            <!-- <input type="text" name="query" placeholder="Search" title="Enter search keyword"> -->
+            <!-- <button type="submit" title="Search"><i class="bi bi-search"></i></button> -->
+            <input type="search" name="search" required>
+            <i class="fa fa-search"></i>
+          </form>
+        </div>
+        <?php
+            
+            //Get the search Keyword
+            $search= $_POST['search'];
+            
+            //SQL Query to get foods based on search keyword
+            $sql="SELECT * FROM tools WHERE itemName LIKE '%$search%'";
+            
+            //Execute the query
+            $res = mysqli_query($conn, $sql);
+            
+            //Count Rows
+            $count =mysqli_num_rows($res);
+            //Check whether food available of not
+            if($count>0)
+            {
+                while($row=mysqli_fetch_assoc($res))
+                {
+			        //get the detalis
+      		        $id=$row['id'];
+     	 		        $title=$row['itemName'];
+      		        $price=$row['price'];
+      		        $dec=$row['description'];
+      		        $img=$row['img'];
+                  ?>
+                    <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+                      <div class="col-lg-4 col-md-6 portfolio-item ">
+                        <div class="portfolio-wrap">
+                          <img src="assets/img/miners/<?php echo $img; ?>" class="img-fluid" alt="">
+                            <div class="portfolio-info">
+                              <h4><?php echo $title; ?></h4>
+                                <div class="portfolio-links">
+		
 
-      </div>
+                                  <a href="assets/img/miners/<?php echo $img; ?>" data-gallery="portfolioGallery" class="portfolio-lightbox" title="<?php echo $title; ?>"><i class="bx bx-plus"></i></a>
+		                              <a class="portfolio-details-lightbox" href="portfolio-details.php?id=<?php echo $id; ?>"class="portfolio-details-lightbox" data-glightbox="type: external" title="Miner Details"><i class="bx bx-link"></i></a>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  <?php
+                }
+            }
+            else
+            {
+                echo"<div class='error'> <h3><strong>No results for:$search </strong></h3><br>Try checking your spelling or use more general terms</div> "; 
+            }
+        ?>
+
+      
     </section><!-- End Miners Section -->
 
   </main><!-- End #main -->
