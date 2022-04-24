35432804439f4128258968ce37c375140ff76c2a
asma1214 <asmabader1214@gmail.com>
2022-04-24 22:25:31 +0300

    user page

--- Login/registerPage.php
+++ Login/registerPage.php
@@ -9,7 +9,7 @@ session_start();
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Pages / Register - NiceAdmin Bootstrap Template</title>
+  <title>Register</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
