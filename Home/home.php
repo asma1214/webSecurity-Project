60d701c1d32ab29a74113ff5f3e266106c48ecb1
Alshams20 <63878117+Alshams20@users.noreply.github.com>
2022-04-25 22:21:29 +0300

    hello.

--- Home/home.php
+++ Home/home.php
@@ -677,19 +677,19 @@ session_start();
               <div class="address">
                 <i class="bi bi-geo-alt"></i>
                 <h4>Location:</h4>
-                <p>A108 Adam Street, New York, NY 535022</p>
+                <p>Eastern Province, Kingdom of Saudi Arabia</p>
               </div>
 
               <div class="email">
                 <i class="bi bi-envelope"></i>
                 <h4>Email:</h4>
-                <p>info@example.com</p>
+                <p>minerminers84@gmail.com</p>
               </div>
 
               <div class="phone">
                 <i class="bi bi-phone"></i>
                 <h4>Call:</h4>
-                <p>+1 5589 55488 55s</p>
+                <p>+966 548625121</p>
               </div>
 
             </div>
