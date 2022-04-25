3dbd59f238ff3f0ce8f110ab4d33fa1c7aa7b8bb
Bushra Al-Hetelah <51024435+BushraH2000@users.noreply.github.com>
2022-04-25 19:58:33 +0300

    sql changepassword

--- Login/editPass.php
+++ Login/editPass.php
@@ -7,9 +7,9 @@
         $ID = rand(1000,9999);
         $name = mysqli_real_escape_string($conn, $_POST['name']);
         $username = mysqli_real_escape_string($conn, $_POST['username']);
-        $password = mysqli_real_escape_string($conn, $_POST['password']);
-        $hashPass= password_hash($password, PASSWORD_DEFAULT);
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $passwordOld = mysqli_real_escape_string($conn, $_POST['password']);
+        $passwordOldNew= password_hash($password, PASSWORD_DEFAULT);
+      
         
         $query2 = "SELECT * FROM users WHERE username = '$username' ";
         $result2 = mysqli_query($conn, $query2);
@@ -23,7 +23,7 @@
         }
         else{
           session_start();
-        $query = "INSERT INTO users (ID, name, username, password, email, Pr) VALUES ('$ID', '$name' , '$username', '$hashPass' , '$email' , 'u' )";
+        $query = "UPDATE users set   WHERE id = {$update_id}";
         define('ROOT_URL', '../Home/home.php');
         if(mysqli_query($conn, $query)){
           $_SESSION['userId'] = $ID;
