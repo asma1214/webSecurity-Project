7c5a307f7c52d0352310bc321dd5e6baee77be01
asma1214 <asmabader1214@gmail.com>
2022-05-05 23:03:22 +0300

    some changes

--- Login/editPass1.php
+++ Login/editPass1.php
@@ -1,42 +1,57 @@
 <?php
 session_start();
 define('USER', 'userProfile.php');
-define('ADMIN', 'userProfile.php');
+define('ADMIN', 'adminProfile.php');
 
 
 require ('../connectDB.php');
-    
 if(isset($_POST['change'])){
     // echo "hello1"; 
     // $newPass1= password_hash($newPass, PASSWORD_DEFAULT);
     // $oldPass= password_hash($oldPass, PASSWORD_DEFAULT);
-
+    
     $currentPass = htmlspecialchars($_POST['currentPass']); 
     $newPass = htmlspecialchars($_POST['newPass']);    
     $rePass = htmlspecialchars($_POST['rePass']);  
     // echo $newPass . "<br>" . $currentPass . "<br>" . $rePass;
-
+    
     $username = $_SESSION['username'];
-             
+    $flag1 = false;   
+    $flag2 = false;   
+    $flag3 = false;     
     $result = mysqli_query($conn, "SELECT * FROM users WHERE username ='$username'");
     $row = mysqli_fetch_array($result);
     $dbPass = $row['password'];
+    
+    // is your current password same as db password?
     if($currentPass == $dbPass){
-        if($newPass == $rePass){
+        $flag1 = true;
+    }
+    // two passwords are matches?
+    if($newPass == $rePass){
+        $flag2 = true;
+    }
+    // ensure the new and old password are not the same
+    if(($flag2) && ($newPass != $dbPass)){
+        $flag3 = true;
+    }
+    // all conditions are met? then change the password
+    if($flag1 && $flag2 && $flag3){
         $query = "UPDATE users SET password='$newPass' WHERE username ='$username'";
         if(mysqli_query($conn, $query)){
+            ?>
+            <script>
+            window.location.href='adminProfile.php';
+            alert('Password sucessfully changed!');
+            </script>
+            <?php
         } else {
             echo 'ERROR: '. mysqli_error($conn);
         }
-        
     }
-//     else {
-//         $Error = "Those passwords didn't match. Try again.";
-//     }
-// }
-// else {
-//     $Error = "Sorry, your password is not correct.";
-// }
+    
+        
+    
 
 
 $pr = $_SESSION['Pr'];
@@ -44,21 +59,32 @@ if($pr == 'u'){
 header('Location: '.USER.'');
 } 
 else{
-    header('Location: '.ADMIN.'');
-  
-}   
-}
-}
-    
-    
-    
-   
-        
-
-
-
-
-
-
-
+    if(!$flag3){ ?>
+    <script>
+    window.location.href='adminProfile.php';
+    alert('your old password and new password are the same!');
+</script>
+ <?php
+    }
+    if(!$flag2){     
 ?>
+<script>
+    window.location.href='adminProfile.php';
+    alert('The two given passwords do not match!');
+</script>
+<?php
+    }
+    else if (!$flag1){ ?>
+
+<script>
+    window.location.href='adminProfile.php';
+    alert('incorrect current password!');
+</script>
+
+<?php
+}
+
+}
+}
+?>
+    
